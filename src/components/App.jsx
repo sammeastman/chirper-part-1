c65df22d9d98a6ed98019bd0ecc89c1eb2d82d59
sammeastman <89807258+sammeastman@users.noreply.github.com>
2021-10-19 10:44:31 -0500

    WIP

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,85 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 
+const App = () => {
+  const [chirps, setChirps] = useState([
+    {
+      username: "Sam",
+      message: "How Can Mirrors Be Real If Our Eyes Aren't Real",
+    },
+    {
+      username: "Sam",
+      message: "How Can Mirrors Be Real If Our Eyes Aren't Real",
+    },
+    {
+      username: "Sam",
+      message: "How Can Mirrors Be Real If Our Eyes Aren't Real",
+    },
+  ]);
 
-class App extends Component {
-    constructor(props) {
-        super(props);
+  const handleUsernameChange = (e) => setUsername(e.target.value);
+  const handleMessage = (e) => setMessage(e.target.value);
+  const handleClick = () => {
+    const newChirp = {
+        username: username,
+        message: message
+    };
+    setChirps([...chirps, newChirp]) 
+  }
 
-        this.state = {
-            isLoggedIn: false,
-            buttonOneText: 'Login',
-            buttonTwoText: 'Sign up',
-            user: null,
-            avatar: null
-        }
-        this.initialState = this.state;
-    }
+  return (
+    <>
+      <h1>Chirper</h1>
 
-    handleSignupDelete = async () => {
-        if (!this.state.isLoggedIn) {
-            this.handleLoginLogout();
-        } else {
-            this.setState(this.initialState);
-        }
-    }
+      <label htmlFor="username-input">Who are you?</label>
+      <input type="text" name="username-input" id=" " />
+      <label htmlFor="message-input">Say it!</label>
+      <textArea name="message-input" cols="40" rows="10" value={message} onChange></textArea>
 
-    handleLoginLogout = async () => {
-        if (!this.state.isLoggedIn) {
-            const { value: name } = await Swal.fire({
-                title: 'Input username',
-                html:
-                `<form>
-                    <div class="form-group">
-                        <label for="username-input">Username</label>
-                        <input type="text" class="form-control" id="username-input" aria-describedby="username" placeholder="Enter your username here" />
-                    </div>
-                    <div class="form-group">
-                        <label for="password-input">Password</label>
-                        <input type="password" class="form-control" id="password-input" aria-describedby="password-reqs" placeholder="Enter your password here" />
-                        <small id="password-reqs" class="form-text text-muted">
-                            Your password is not saved, submitted, has no length requirements, or character requirements. Hooray for empty client-side validation!
-                        </small>
-                    </div>
-                </form>`,
-                focusConfirm: false,
-                preConfirm: () => {
-                  return document.getElementById('username-input').value.replace(/[^a-zA-Z0-9]/g, "").substring(0,15);
-                }
-            });
-            if (name) {
-                this.setState({
-                    user: name,
-                    avatar: 'https://picsum.photos/128',
-                    isLoggedIn: true,
-                    buttonOneText: 'Logout',
-                    buttonTwoText: name ? `Delete @${name}` : 'Haxx, you logged in without a real account because this is using client-side validation!'
-                });
-            }
-        } else {
-            this.setState(this.initialState);
-        }
-    }
+      {chirps.map((chirp) => (
+        <>
+          <div>{chirp.username}</div>
+          <p>{chirp.message}</p>
+        </>
+      ))}
+    </>
+  );
+};
 
-    render() {
-        return (
-            <Container>
-                <Row className="justify-content-end margin-top-10 margin-bot-20 hr-thin pad-bot-10 vr-thin">
-                    <Col md={2}><Button onClick={this.handleLoginLogout} className="badge-pill vr-thin" variant="outline-info">{this.state.buttonOneText}</Button></Col>
-                    <Col md={2}><Button onClick={this.handleSignupDelete} className="badge-pill vr-thin" variant="info">{this.state.buttonTwoText}</Button></Col>
-                </Row>
-                <Row>
-                    <Col md={2} className="justify-content-md-left">
-                        <LeftSidebar App={this.state} />
-                    </Col>
-                    <Col md={7} className="justify-content-md-center text-center vr-thin">
-                        <ChirpPanel App={this.state} />
-                    </Col>
-                    <Col md={3}>
-                        <TrendsPane />
-                    </Col>
-                </Row>
-            </Container>
-        );
-    }
-}
 export default App;
